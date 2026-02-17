cec7f2340622e5b9e4ecaca29ceae582ffb381ec
Abhijeetpanwar0001 <abhiyant3355panwar@gmail.com>
2026-02-17 15:08:18 +0530

    Create Q34.c

---
+++ DSA100DAYSOFCODE/DAY17/Q34.c
@@ -0,0 +1,24 @@
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+int maxSubArray(int* nums, int numsSize) {
+    
+    int max_current = nums[0];
+    int max_global = nums[0];
+    
+    for(int i = 1; i < numsSize; i++) {
+        
+        // Either extend the previous subarray or start new from current element
+        if(max_current + nums[i] > nums[i]) {
+            max_current = max_current + nums[i];
+        } else {
+            max_current = nums[i];
+        }
+        
+        
+        if(max_current > max_global) {
+            max_global = max_current;
+        }
+    }
+    
+    return max_global;
+}
