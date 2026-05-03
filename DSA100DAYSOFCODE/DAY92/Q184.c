45d97a5227b4d7747c8d26d569464765fd08d686
Abhijeetpanwar0001 <abhiyant3355panwar@gmail.com>
2026-05-03 09:35:14 +0530

    Update Q184.c

--- DSA100DAYSOFCODE/DAY92/Q184.c
+++ DSA100DAYSOFCODE/DAY92/Q184.c
@@ -1,42 +1,50 @@
+#include <stdlib.h>
 
+// Swap
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-
-int partition(int nums[], int left, int right) {
-    int pivot = nums[right];
-    int i = left;
-
-    for (int j = left; j < right; j++) {
-        if (nums[j] <= pivot) {
-            swap(&nums[i], &nums[j]);
-            i++;
-        }
-    }
-    swap(&nums[i], &nums[right]);
-    return i;
-}
-
-
-int quickSelect(int nums[], int left, int right, int k_smallest) {
-    if (left == right)
-        return nums[left];
-
-    int pivotIndex = partition(nums, left, right);
-
-    if (pivotIndex == k_smallest)
-        return nums[pivotIndex];
-    else if (pivotIndex > k_smallest)
-        return quickSelect(nums, left, pivotIndex - 1, k_smallest);
-    else
-        return quickSelect(nums, pivotIndex + 1, right, k_smallest);
-}
-
-
 int findKthLargest(int* nums, int numsSize, int k) {
-    
-    return quickSelect(nums, 0, numsSize - 1, numsSize - k);
+    int left = 0, right = numsSize - 1;
+    int target = numsSize - k;
+
+    while (left <= right) {
+        // Random pivot
+        int pivot = nums[left + rand() % (right - left + 1)];
+
+        // 3-way partition
+        int low = left, mid = left, high = right;
+
+        while (mid <= high) {
+            if (nums[mid] < pivot) {
+                swap(&nums[low], &nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] > pivot) {
+                swap(&nums[mid], &nums[high]);
+                high--;
+            }
+            else {
+                mid++;
+            }
+        }
+
+        // Now:
+        // [left ... low-1] < pivot
+        // [low ... high] == pivot
+        // [high+1 ... right] > pivot
+
+        if (target >= low && target <= high)
+            return nums[target];
+        else if (target < low)
+            right = low - 1;
+        else
+            left = high + 1;
+    }
+
+    return -1;
 }
